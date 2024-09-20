6883bbaad5ba3b6f139105399dea1bcea8321c6d
eduardopilnik <eduardopilnik@localhost>
2024-09-20 14:28:03 -0500

    Added Node Label

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeLabels.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeLabels.java
@@ -2,5 +2,8 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
-	root, //other nodes
+	root,
+	Bunker,
+	
+	//other nodes
 }
