0cdfc757f5d0af8c5dbff63b1293ab6e622b924d
eduardopilnik <eduardopilnik@localhost>
2024-10-27 16:18:49 -0500

    pushing edgeBuilder Framework

--- MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
@@ -2,6 +2,9 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
+	leaveBunker,redBerries,blueBerries,stopForCar,leaveCar,stayOnCoast,stayOnForest,
+	followWater,dontFollowWater,petAnimal,runFromAnimal,goWithGroup,stayAlone,leaveBody,stayWithBody,
+	searchBody,cry,complyWithParty,runFromParty,goNorth,goSouth,fixCar;
 	
 }
 
--- MyClassProject/src/myclassproject/mystorygraph/MyEdgeBuilder.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyEdgeBuilder.java
@@ -2,7 +2,9 @@ package myclassproject.mystorygraph;
 
 import java.util.List;
 
+import com.playerInput.MenuChoice;
 import com.storygraph.BuilderMethod;
+import com.storygraph.Edge;
 import com.storygraph.Node;
 import com.storygraph.NodeBuilder;
 
@@ -21,97 +23,302 @@ public class MyEdgeBuilder extends NodeBuilder {
 	 * The method should add the edges of the node one by one. 
 	 * These methods must have a BuilderMethod annotation.
 	 */
-	@BuilderMethod
-	public void rootEdges() {
+	//@BuilderMethod
+	//public void rootEdges() {
 		//Example:
 		//var root = get(NodeLabels.root.toString());
 		//var choice = new MenuChoice(MenuChoice.Options.Start);
 		//var nextNode = get(NodeLabels.atCottage.toString());
 		//root.add(new Edge(choice, nextNode));
-	}
+	//}
 	
-	@BuilderMethod
-	public void stayInBunkerEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void leaveBunkerEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void atStoreEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void atHospitalEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void atForestEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void purpleBerriesEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void redBerriesEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void carInForestEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void leaveStoreEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void leaveHospitalEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void atCoastEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void stopForCarEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void moreForestEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void petAnimalEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void OasisEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void runFromAnimalEdges() {
-		
-	}
-	
-	@BuilderMethod
-	public void followWaterTrailEdges() {
-		
-	}
+	   @BuilderMethod
+	    public void rootEdges() {
+	        var root = get("root");
+	        var choice = new MenuChoice(MenuChoice.Options.Start);
+	        var nextNode = get("atCottage");
+	        root.add(new Edge(choice, nextNode));
+	    }
+
+	    // Automatically generated edge methods for each node label
+
+
+	    @BuilderMethod
+	    public void stayinbunkerEdges() {
+	        var node = get("stayinbunker");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void leavebunkerEdges() {
+	        var node = get("leavebunker");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void atstoreEdges() {
+	        var node = get("atstore");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void athospitalEdges() {
+	        var node = get("athospital");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void atforestEdges() {
+	        var node = get("atforest");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void purpleberriesEdges() {
+	        var node = get("purpleberries");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void redberriesEdges() {
+	        var node = get("redberries");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void carinforestEdges() {
+	        var node = get("carinforest");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void leavestoreEdges() {
+	        var node = get("leavestore");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void leavehospitalEdges() {
+	        var node = get("leavehospital");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void atcoastEdges() {
+	        var node = get("atcoast");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void stopforcarEdges() {
+	        var node = get("stopforcar");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void moreforestEdges() {
+	        var node = get("moreforest");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void petanimalEdges() {
+	        var node = get("petanimal");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void oasisEdges() {
+	        var node = get("oasis");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void runfromanimalEdges() {
+	        var node = get("runfromanimal");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void followwatertrailEdges() {
+	        var node = get("followwatertrail");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void seehutEdges() {
+	        var node = get("seehut");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void lostinforestEdges() {
+	        var node = get("lostinforest");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void safetyconversationEdges() {
+	        var node = get("safetyconversation");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void dyingpersonEdges() {
+	        var node = get("dyingperson");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void northEdges() {
+	        var node = get("north");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void staywithdyingpersonEdges() {
+	        var node = get("staywithdyingperson");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void leavedyingpersonEdges() {
+	        var node = get("leavedyingperson");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void searchbodyEdges() {
+	        var node = get("searchbody");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void sitthereandcryEdges() {
+	        var node = get("sitthereandcry");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void afterdeathEdges() {
+	        var node = get("afterdeath");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void gowithpartyEdges() {
+	        var node = get("gowithparty");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void goaloneEdges() {
+	        var node = get("goalone");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void complywithpartyEdges() {
+	        var node = get("complywithparty");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void runawayfrompartyEdges() {
+	        var node = get("runawayfromparty");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void southEdges() {
+	        var node = get("south");
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void lastcarEdges() {
+	        var node = get("lastcar");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void dontgotocityEdges() {
+	        var node = get("dontgotocity");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
+
+	    @BuilderMethod
+	    public void gotocityEdges() {
+	        var node = get("gotocity");
+	        var choice = new MenuChoice(MenuChoice.Options.Choose);
+	        var nextNode = get("someNextNode");  // Specify appropriate next node based on game logic
+	        node.add(new Edge(choice, nextNode));
+	    }
 }
